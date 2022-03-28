a27bdbe6d6a582ba285883fb6767418c9489e4ea
Ahmed <sw.ahmedadel@gmail.com>
2022-03-28 08:50:09 +0200

    git all tours

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const server = http.createServer(app);
 
 //const {loadToursData} = require('./models/tours.models');
 const {startMongo} = require('./services/mongo');
+const {loadToursData} = require('../src/models/tours.models')
 
-async function startServer() {
- 
-//  await loadToursData();
+async function startServer() { 
   await startMongo();
+  await loadToursData();
   server.listen(PORT , () => {
     console.log('server is running');
   })
