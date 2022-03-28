5c938f99ace747cf7f8a5cdc3a73e6fcff8f83bb
Ahmed <sw.ahmedadel@gmail.com>
2022-03-28 09:06:43 +0200

    delete reading tours.json code to start build the api

--- src/routes/tours/tours.routes.js
+++ src/routes/tours/tours.routes.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const toursRtoure = express.Router();
-const {
-  httpGetAllTours,
-} = require('./tours.controller');
 
-toursRtoure.get('/' , httpGetAllTours);
 
 module.exports= toursRtoure;
--- src/server.js
+++ src/server.js
@@ -4,13 +4,12 @@ require('dotenv').config();
 const PORT=process.env.PORT;
 const server = http.createServer(app);
 
-//const {loadToursData} = require('./models/tours.models');
+
 const {startMongo} = require('./services/mongo');
-const {loadToursData} = require('../src/models/tours.models')
 
 async function startServer() { 
   await startMongo();
-  await loadToursData();
+
   server.listen(PORT , () => {
     console.log('server is running');
   })
