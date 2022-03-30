7b73d2c0b7d23418de75aef16b03048bd5d45ff1
Ahmed <sw.ahmedadel@gmail.com>
2022-03-30 09:58:27 +0200

    Handling Unhandled Routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const api = require('./routes/api')
+const api = require("./routes/api");
 app.use(express.json());
 
-app.use('/v1' , api);
- 
+app.use("/v1", api);
+app.all("*", (req, res) => {
+  return res.status(404).json({
+    status: "fail",
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
