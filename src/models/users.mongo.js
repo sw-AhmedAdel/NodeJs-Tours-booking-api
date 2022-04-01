52294e0f702854e5c4e57dfeadffe613595d819a
Ahmed <sw.ahmedadel@gmail.com>
2022-04-01 14:17:40 +0200

    add mongoose middleware

--- src/models/users.mongo.js
+++ src/models/users.mongo.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt =require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -30,6 +31,14 @@ const userSchema = new mongoose.Schema({
 
 
 
+
+
+userSchema.pre('save' , async function(next) {
+  const user = this;
+  if(user.isModified  ('password')) {
+    user.password = await bcrypt.hash(user.password , 8 );
+  }
+})
 
 const users = mongoose.model('user' , userSchema);
 module.exports = users;
