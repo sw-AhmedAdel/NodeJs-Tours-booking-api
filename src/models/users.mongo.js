bf86f2d9204f9d6ffd91dfbed57c0e04f2ea4ce3
Ahmed <sw.ahmedadel@gmail.com>
2022-04-01 14:26:54 +0200

    add findByrCedenitals and getAuthToken

--- src/models/users.mongo.js
+++ src/models/users.mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt =require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,14 +26,40 @@ const userSchema = new mongoose.Schema({
   passwordConfirm : {
     type:String ,
     required: [true, 'please provide your a password'],
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    },
+  }]
 }, {
   timestamps : true,
 })
 
 
 
+const secret = process.env.JWT_SECRET;
+userSchema.methods.getAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign( {_id : user._id.toString()} , secret , {expiresIn:'7 days'} );
+  user.tokens.push({token});
+  await user.save();
+  return token;
+}
 
+userSchema.statics.findByrCedenitals =  await function (email , password) {
+  const user = await users.findOne(email);
+  if(!user) {
+    return false;
+  }
+
+  const isMatch = await bcrypt.compare(password , user.password);
+  if(!isMatch) {
+    return false;
+  }
+  return user;
+}
 
 userSchema.pre('save' , async function(next) {
   const user = this;
