5bddbd6ffed499ce28845c6d602a5c12ed2c1329
Ahmed <sw.ahmedadel@gmail.com>
2022-04-04 11:30:27 +0200

    add xss

--- src/app.js
+++ src/app.js
@@ -5,14 +5,27 @@ const appError = require('../src/services/class.err.middleware');
 const {handlingErrorMiddleware} = require('../src/services/handling.error');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+
 const limiter = rateLimit({
   max: 100 ,
   windowMs: 60 * 60 * 1000, // 1 hour
   message:'To many requests from this api/ please try again an hour'
 })
-app.use(limiter)
-app.use(helmet());
+app.use(limiter) // limit request
+app.use(helmet()); // secure http headers
 app.use(express.json());
+
+// try to login using this email "email":{"$gt": ""}, it will work so we need to 
+// data sanitization against nosql query injection  >  npm i express-mongo-sanitize
+app.use(mongoSanitize());
+//Imagine that an attacker would try to insert some malicious HTML code with some JavaScript code attached to it.
+
+//If that would then later be injected into our HTML site, it could really create some damage then.
+
+//Using this middleware, we prevent that basically by converting all these HTML symbols.
+app.use(xss());
 app.use("/v1", api);
 
 app.all("*", (req, res , next) => {
