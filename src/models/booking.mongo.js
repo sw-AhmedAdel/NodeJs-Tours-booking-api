549267a19b7109c6616edc8770d81e98a95400f5
Ahmed <sw.ahmedadel@gmail.com>
2022-04-10 21:25:48 +0200

    add booking model

--- src/models/booking.mongo.js
+++ src/models/booking.mongo.js
@@ -1,6 +1,31 @@
 const mongoose  = require('mongoose');
 const bookingSchema = new mongoose.Schema({
 
+  tour : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref:'tour',
+    required:[true ,' Booking must belong to a tour']
+  },
+  user : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref:'user',
+    required:[true ,' Booking must belong to a user']
+  },
+  price:{
+    type: Number,
+    required: [true, 'Booking must have a price']
+  },
+}, {
+  timestamps: true,
+})
+
+bookingSchema.pre(/^find/ , function(next) {
+  this.populate({
+    path:'user',
+  }).populate({
+    path:'tour',
+    select:'name , price'
+  })
 })
 
 const bookings = mongoose.model('booking' , bookingSchema);
